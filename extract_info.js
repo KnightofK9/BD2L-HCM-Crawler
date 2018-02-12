2e26e9c765a61c2760fb2ed06de34c22db43071f
Phan Truong Thanh <knightofk9@gmail.com>
2018-02-12 10:22:11 +0700

    remove js

--- extract_info.js
+++
@@ -1,9 +0,0 @@
-let fs = require('fs');
-
-let contents = fs.readFileSync('web.txt', 'utf8');
-let reg_exp = /http:[^"]+\.m3u8/g
-let array
-while(array = reg_exp.exec(contents)){
-  console.log(array);
-  break;
-}
