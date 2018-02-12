65f6d592862f4d8a900da7d7f370e38ae35987f7
Phan Truong Thanh <knightofk9@gmail.com>
2018-02-12 10:02:16 +0700

    no message

--- extract_info.py
+++ extract_info.py
@@ -1,12 +1,15 @@
 import re
 
+
+
 def main():
     data = None
     with open('web.txt', 'r') as info_file:
         data = info_file.read()
     reg_exp = r'http:[^"]+\.m3u8'
-    result = re.findall(reg_exp,data)
-    print "  \n".join(result)
+    result = re.findall(reg_exp, data)
+    for i in range(0, len(result) - 1):
+        print  "{0:02d} ".format(i) + result[i] + "  "
 
 
 main()
